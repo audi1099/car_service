8f60bdc9a3ddfb669f27ebc698af2a4665ab9fc3
В К <vk@Vs-MacBook-Air.local>
2025-01-30 20:01:32 +0300

    Create admin models

--- workshop/admin.py
+++ workshop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Client, Car, RepairOrder
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone", "email")
+    search_fields = ("first_name", "last_name", "phone", "email")
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("brand", "model", "year", "owner")
+    search_fields = ("brand", "model", "vin")
+    list_filter = ("year",)
+
+@admin.register(RepairOrder)
+class RepairOrderAdmin(admin.ModelAdmin):
+    list_display = ("car", "status", "created_at")
+    search_fields = ("car__brand", "car__model")
+    list_filter = ("status", "created_at")
+    ordering = ("-created_at",)
